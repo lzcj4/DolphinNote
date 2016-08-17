685b7f6dff93e426b97bde417a65af9b76cb9d96
lzcj4@163.com <lzcj4@163.com>
2016-08-17 16:45:47 +0800

    Add PQ com  protocol

--- sdk/src/main/java/com/tannuo/sdk/device/protocol/PQProtocol.java
+++ sdk/src/main/java/com/tannuo/sdk/device/protocol/PQProtocol.java
@@ -12,16 +12,15 @@ import java.util.List;
  * Created by Nick_PC on 2016/6/30.
  */
 public class PQProtocol extends ProtocolBase {
-    private final byte FRAME_HEADER = 0x02;
-    private final byte FRAME_END = 0x06;
-    private final byte FRAME_BYTES = 0x3E;//62 bytes
-    private final byte FRAME_POINTS = 6;
-    private final byte POINT_BYTES = 10;
+    private final byte FRAME_HEADER1 = 0x55;
+    private final byte FRAME_HEADER2 = 0x54;
+    private final byte POINT_BYTES = 5;
+    private final byte FRAME_BYTES = 10;
 
-    private static final byte ACTION_NONE = 0x00;
-    private final byte ACTION_DOWN = 0x07;
-    private final byte ACTION_MOVE = ACTION_NONE;
-    private final byte ACTION_UP = 0x04;
+    private final byte ACTION_NONE = 0x0;
+    private final byte ACTION_DOWN = (byte) 0x81;
+    private final byte ACTION_MOVE = (byte) 0x82;
+    private final byte ACTION_UP = (byte) 0x84;
 
     public PQProtocol() {
         TouchPoint.setActions(ACTION_DOWN, ACTION_MOVE, ACTION_UP);
@@ -33,15 +32,7 @@ public class PQProtocol extends ProtocolBase {
             throw new IllegalArgumentException();
         }
 
-        DataLog.getInstance().writeInData(data);
-        DataLog.getInstance().writeInLineData(data);
         reset();
-
-//        02 07 00 90 52 8a 13 38 00 66 00 00 01 00 00 00
-//        00 00 00 00 00 00 02 00 00 00 00 00 00 00 00 00
-//        03 00 00 00 00 00 00 00 00 00 04 00 00 00 00 00
-//        00 00 00 00 05 00 00 00 00 00 00 00 06
-
         byte[] totalData = combineBytes(lastUnhandledBytes, data);
         if (lastUnhandledBytes.length != 0) {
             lastUnhandledBytes = new byte[0];
@@ -57,34 +48,44 @@ public class PQProtocol extends ProtocolBase {
             }
 
             //Check header
-            if (!(totalData[index++] == FRAME_HEADER)) {
+            if (!(totalData[index++] == FRAME_HEADER1 && totalData[index++] == FRAME_HEADER1)) {
                 i++;
                 continue;
             }
 
-            for (int j = 0; j < FRAME_POINTS && index < len; j++) {
-                TouchPoint point = new TouchPoint();
-                byte action = totalData[index++];
-                if (action == ACTION_NONE) {
-                    index += POINT_BYTES - 1;
-                    continue;
-                }
-                point.setActionByDevice(action); // byte:0
-                point.setId(totalData[index++]);// byte:1
-                point.setX(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:2,3
-                point.setY(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:4,5
-                point.setWidth(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:6,7
-                point.setHeight(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:8,9
-                mPoints.add(point);
+
+            TouchPoint point = new TouchPoint();
+            byte action = totalData[index++];
+            if (action == ACTION_NONE) {
+                index += POINT_BYTES - 1;
+                continue;
             }
+            point.setActionByDevice(action); // byte:0
+            //point.setId(totalData[index++]);// byte:1
+            point.setX(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:2,3
+            point.setY(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:4,5
+//            point.setWidth(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:6,7
+//            point.setHeight(DataUtil.bytesShortLittleEndian(totalData[index++], totalData[index++]));// byte:8,9
+            mPoints.add(point);
+            index++;
             // Collections.sort(mPoints, (lhs, rhs) -> lhs.getId() - rhs.getId());
-            int frameEnd = totalData[index++];
-            if (frameEnd == FRAME_END) {
+            byte checksum = totalData[index++];
+
+            byte[] frameData = Arrays.copyOfRange(totalData, index - 10, index - 1);
+            if (checkChecksum(frameData, checksum)) {
                 Logger.i(TAG, "getFactory frame end ");
             }
             i = index;
         }
         return result;
+    }
+
+    private boolean checkChecksum(byte[] frameData, byte checksum) {
+        byte sum = 0;
+        for (byte b : frameData) {
+            sum += b;
+        }
+        return sum == checksum;
     }
 
     @Override
