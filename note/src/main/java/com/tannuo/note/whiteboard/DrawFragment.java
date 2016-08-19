189e47772d7cfa634c717881a15514bc3fc0debc
lzcj4@163.com <lzcj4@163.com>
2016-08-19 18:32:12 +0800

    Fixed draw and rubber issue

--- note/src/main/java/com/tannuo/note/whiteboard/DrawFragment.java
+++ note/src/main/java/com/tannuo/note/whiteboard/DrawFragment.java
@@ -178,7 +178,6 @@ public class DrawFragment extends Fragment implements TouchPointListener {
                         } else {
                             mLineSmooth.drawLine(list, true);
                         }
-
                     }
                 }
                 break;
@@ -212,8 +211,6 @@ public class DrawFragment extends Fragment implements TouchPointListener {
         for (int i = 0; i < groups.size(); i++) {
             List<TouchPoint> list = groups.valueAt(i);
             //drawView.drawPoints(list);
-            //drawSmoothLine(list);
-            //drawLine(list);
             if (list.size() > 0 && list.get(0).isRubber()) {
                 for (TouchPoint point : list) {
                     drawRubber(point);
@@ -227,21 +224,6 @@ public class DrawFragment extends Fragment implements TouchPointListener {
     SparseArray<TouchPoint> historyMap = new SparseArray<>();
     Path mDrawPath = new Path();
 
-    private void drawSmoothLine(List<TouchPoint> points) {
-        if (null == mBmpCanvas || points.isEmpty()) {
-            return;
-        }
-
-        int len = points.size();
-        for (int i = 0; i < len; i++) {
-            TouchPoint point = points.get(i);
-            TouchPoint lastPoint = historyMap.get(point.getId());
-
-            startDraw(lastPoint, point);
-            historyMap.put(point.getId(), point);
-        }
-        drawBitmap();
-    }
 
     private void drawLine(List<TouchPoint> points) {
         if (null == mBmpCanvas || points.isEmpty()) {
@@ -351,161 +333,6 @@ public class DrawFragment extends Fragment implements TouchPointListener {
     //The stroke width (default will be 7 pixels).
     private int STROKE_WIDTH = 5;
 
-    // A value is used in a lowpass filter to calculate the velocity between two points.
-    private float VELOCITY_FILTER_WEIGHT = 0.2f;
-
-    // Those of values present for : ---startPoint---previousPoint----currentPoint
-    private Point previousPoint;
-    private Point startPoint;
-    private Point currentPoint;
-
-    // contain the last velocity. Will be used to calculate the Stroke Width
-    private float lastVelocity;
-
-    // contain the last stroke width. Will be used to calculate the Stroke Width
-    private float lastWidth;
-
-    public void startDraw(TouchPoint lastPoint, TouchPoint firstPoint) {
-        if (null == lastPoint) {
-            // In Action down  currentPoint, previousPoint, startPoint will be set at the same point.
-            currentPoint = new Point(firstPoint.getX(), firstPoint.getY(), System.currentTimeMillis());
-            previousPoint = currentPoint;
-            startPoint = previousPoint;
-        } else {
-            // Those of values present for : ---startPoint---previousPoint----currentPoint-----
-            startPoint = previousPoint;
-            previousPoint = currentPoint;
-            currentPoint = new Point(firstPoint.getX(), firstPoint.getY(), System.currentTimeMillis());
-
-            // Calculate the velocity between the current point to the previous point
-            float velocity = currentPoint.velocityFrom(previousPoint);
-
-            // A simple lowpass filter to mitigate velocity aberrations.
-            velocity = VELOCITY_FILTER_WEIGHT * velocity + (1 - VELOCITY_FILTER_WEIGHT) * lastVelocity;
-
-            // Caculate the stroke width based on the velocity
-            float strokeWidth = getStrokeWidth(velocity);
-
-
-            // Draw line to the canvasBmp canvas.
-            drawLine(mBmpCanvas, mLinePaint, lastWidth, strokeWidth);
-
-            // Tracker the velocity and the stroke width
-            lastVelocity = velocity;
-            lastWidth = strokeWidth;
-
-        }
-        if (null != firstPoint && firstPoint.getIsUp()) {
-            startPoint = previousPoint;
-            previousPoint = currentPoint;
-            currentPoint = new Point(firstPoint.getX(), firstPoint.getY(), System.currentTimeMillis());
-            drawLine(mBmpCanvas, mLinePaint, lastWidth, 0);
-        }
-    }
-
-    private float getStrokeWidth(float velocity) {
-        return STROKE_WIDTH - velocity;
-    }
-
-    // Generate mid point values
-    private Point midPoint(Point p1, Point p2) {
-        return new Point((p1.x + p2.x) / 2.0f, (p1.y + p2.y) / 2, (p1.time + p2.time) / 2);
-    }
-
-    private void drawLine(Canvas canvas, Paint paint, float lastWidth, float currentWidth) {
-        Point mid1 = midPoint(previousPoint, startPoint);
-        Point mid2 = midPoint(currentPoint, previousPoint);
-        draw(canvas, mid1, previousPoint, mid2, paint, lastWidth, currentWidth);
-    }
-
-
-    /**
-     * This method is used to draw a smooth line. It follow "Bézier Curve" algorithm (it's Quadratic curves).
-     * </br> For reference, you can see more detail here: <a href="http://en.wikipedia.org/wiki/B%C3%A9zier_curve">Wiki</a>
-     * </br> We 'll draw a  smooth curves from three points. And the stroke size will be changed depend on the start width and the end width
-     *
-     * @param canvas       : we 'll draw on this canvas
-     * @param p0           the start point
-     * @param p1           mid point
-     * @param p2           end point
-     * @param paint        the paint is used to draw the points.
-     * @param lastWidth    start stroke width
-     * @param currentWidth end stroke width
-     */
-    private void draw(Canvas canvas, Point p0, Point p1, Point p2, Paint paint, float lastWidth, float currentWidth) {
-        float xa, xb, ya, yb, x, y;
-        float different = (currentWidth - lastWidth);
-
-        for (float i = 0; i < 1; i += 0.01) {
-            // This block of code is used to calculate next point to draw on the curves
-            xa = getPt(p0.x, p1.x, i);
-            ya = getPt(p0.y, p1.y, i);
-            xb = getPt(p1.x, p2.x, i);
-            yb = getPt(p1.y, p2.y, i);
-
-            x = getPt(xa, xb, i);
-            y = getPt(ya, yb, i);
-            //
-
-            // reset strokeWidth
-            paint.setStrokeWidth(lastWidth + different * (i));
-            canvas.drawPoint(x, y, paint);
-        }
-    }
-
-
-    // This method is used to calculate the next point cordinate.
-    private float getPt(float n1, float n2, float perc) {
-        float diff = n2 - n1;
-        return n1 + (diff * perc);
-    }
-
-
-    /**
-     * This method is used to save the bitmap to an output stream
-     *
-     * @param outputStream
-     */
-//    public void save(OutputStream outputStream) {
-//        Bitmap bitmap = getDrawingCache();
-//        if (bitmap != null) {
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 80, outputStream);
-//        }
-//    }
-
-
-    public class Point {
-        public final float x;
-        public final float y;
-        public final long time;
-
-        public Point(float x, float y, long time) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-        }
-
-        /**
-         * Caculate the distance between current point to the other.
-         *
-         * @param p the other point
-         * @return
-         */
-        private float distanceTo(Point p) {
-            return (float) (Math.sqrt(Math.pow((x - p.x), 2) + Math.pow((y - p.y), 2)));
-        }
-
-
-        /**
-         * Caculate the velocity from the current point to the other.
-         *
-         * @param p the other point
-         * @return
-         */
-        public float velocityFrom(Point p) {
-            return distanceTo(p) / (this.time - p.time);
-        }
-    }
 
     private class LineSmooth {
         BezierSmooth mSmooth = new BezierSmooth();
@@ -524,39 +351,51 @@ public class DrawFragment extends Fragment implements TouchPointListener {
 
         TouchPoint mLastPoint = null;
 
-        private void drawLine(List<TouchPoint> points, boolean isUp) {
+        private synchronized void drawLine(List<TouchPoint> points, boolean isUp) {
             if (null == points || points.size() == 0) {
                 return;
             }
+
             if (mLastPoint == null) {
                 mLastPoint = points.get(0);
+                mDrawPath.reset();
             }
 
-            DrawUtil.getInstance().moveTo(mDrawPath, mLastPoint.getX(), mLastPoint.getY(), mPaintWidth, mPaintHeight);
+            boolean isDrawed = false;
             int[][] rawPointArray = toPointArray(points);
             int len = points.size();
             for (int i = 0; i < len; i++) {
                 Logger.d(TAG, String.format("/-----  rawPointArray:x=%s,y=%s,id=%s", rawPointArray[i][0], rawPointArray[i][1], rawPointArray[i][2]));
-                int[][] pointArray = mSmooth.Point_Filter(rawPointArray[i], false);
-                if (null != pointArray) {
-                    for (int j = 0; j < pointArray.length; j++) {
-                        Logger.d(TAG, String.format("/++++ translate pointArray:x=%s,y=%s,id=%s", pointArray[j][0], pointArray[j][1], pointArray[j][2]));
-                        Logger.d(TAG, String.format("/****  translate point:x=%s,y=%s,id=%s",
-                                TouchPoint.getScaleX(pointArray[j][0]), TouchPoint.getScaleY(pointArray[j][1]), pointArray[j][2]));
-                        mDrawPath.lineTo(TouchPoint.getScaleX(pointArray[j][0]), TouchPoint.getScaleY(pointArray[j][1]));
-                        mLastPoint = new TouchPoint();
-                        mLastPoint.setId(pointArray[j][2]);
-                        mLastPoint.setX((short) pointArray[j][0]);
-                        mLastPoint.setY((short) pointArray[j][1]);
-                    }
-                    if (isUp) {
-                        mLastPoint = null;
-                    }
+                int[][] pointArray = mSmooth.Point_Filter(rawPointArray[i], isUp);
+                if (null == pointArray) {
+                    continue;
+                }
+
+                float moveX = mLastPoint.getX(), moveY = mLastPoint.getY();
+                mDrawPath.moveTo(moveX, moveY);
+                for (int j = 0; j < pointArray.length; j++) {
+                    Logger.d(TAG, String.format("/++++ translate pointArray:x=%s,y=%s,id=%s", pointArray[j][0], pointArray[j][1], pointArray[j][2]));
+                    Logger.d(TAG, String.format("/****  translate point:x=%s,y=%s,id=%s",
+                            TouchPoint.getScaleX(pointArray[j][0]), TouchPoint.getScaleY(pointArray[j][1]), pointArray[j][2]));
+                    mDrawPath.lineTo(TouchPoint.getScaleX(pointArray[j][0]), TouchPoint.getScaleY(pointArray[j][1]));
+                    mLastPoint = new TouchPoint();
+                    mLastPoint.setId(pointArray[j][2]);
+                    mLastPoint.setX((short) pointArray[j][0]);
+                    mLastPoint.setY((short) pointArray[j][1]);
+                    isDrawed = true;
                 }
             }
-            mBmpCanvas.drawPath(mDrawPath, mLinePaint);
-            drawBitmap();
-            mDrawPath.reset();
+
+            if (isDrawed) {
+                mBmpCanvas.drawPath(mDrawPath, mLinePaint);
+                drawBitmap();
+                mDrawPath.reset();
+            }
+
+            if (isUp) {
+                mLastPoint = null;
+                mDrawPath.reset();
+            }
         }
 
     }
